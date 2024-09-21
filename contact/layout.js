3faa699149f61c337b3cc8e07fcef229aa3191a2
sufiyan <sufiyannsalmann@gmail.com>
2024-09-21 21:45:57 +0500

    Revert "Contact Task"
    This reverts commit de4e1e2db6ff14da7225d040a90ae03da5288924.

--- contact/layout.js
+++
@@ -1,9 +0,0 @@
-// app/layout.js (Next.js with app directory structure)
-export default function ContactLayout({ children }) {
-    return (
-      <div className="min-h-screen flex flex-col">  
-        {children}
-      </div>
-    );
-  }
-  
